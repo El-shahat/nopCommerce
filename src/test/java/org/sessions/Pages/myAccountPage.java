19be54a49a5dec7fb6f9a8aa61ef132e42e73315
ahmedmohamedshahat <ahmedshahat852@gmail.com>
2025-10-06 18:06:42 +0300

    nopCommerce testing project

--- src/test/java/org/sessions/Pages/myAccountPage.java
+++ src/test/java/org/sessions/Pages/myAccountPage.java
@@ -3,9 +3,6 @@ package org.sessions.Pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-
-import javax.xml.xpath.XPath;
-
 import java.util.List;
 
 import static  org.sessions.StepDefinations.Hooks.driver;
