488f6c9f4dd6271a7a4e36338a6dde054326de15
ahmedmohamedshahat <ahmedshahat852@gmail.com>
2025-10-08 16:09:56 +0300

    nopCommerce testing project

--- src/test/java/org/sessions/StepDefinations/checkout.java
+++ src/test/java/org/sessions/StepDefinations/checkout.java
@@ -5,7 +5,6 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.sessions.Pages.CartPage;
